40018ea9c36e859ff26ef625a8425de09b798033
Benjamin Chislett <chislett.ben@gmail.com>
2024-12-30 21:51:54 -0500

    timeline chart

--- src/ui/dash.tsx
+++ src/ui/dash.tsx
@@ -27,13 +27,12 @@ function dollarStrFormatter(s: string): string {
     return "$" + Number.parseFloat(s).toFixed(2);
 }
 
-const ApexChart = (expenseSummaries: ExpenseSummary[], bar: boolean, collectBottomLevel: boolean) => {
+const ExpenseChart = (expenseSummaries: ExpenseSummary[], bar: boolean, collectBottomLevel: boolean) => {
     const height = 520;
     let series: any = [];
     let options: any = {};
     let categories: string[] = [];
     let categoryForAccount: { [account: string]: string } = {};
-    let i = 0;
     for (const expense of expenseSummaries) {
         let category = expense.account.split(':').slice(0, 2).join(':');
         if (collectBottomLevel && category == expense.account) {
@@ -92,7 +91,7 @@ const ApexChart = (expenseSummaries: ExpenseSummary[], bar: boolean, collectBott
             },
             xaxis: {
                 categories,
-                labels: { formatter: (s: string) => s.split(':').slice(-1)[0]}
+                labels: { formatter: (s: string) => s.split(':').slice(-1)[0] }
             },
             yaxis: {
                 labels: { formatter: expenseSummaries.length === 0 ? (_1: any, _2: any) => "no data" : dollarFormatter }, // two decimal points
@@ -144,12 +143,122 @@ const ApexChart = (expenseSummaries: ExpenseSummary[], bar: boolean, collectBott
     );
 }
 
+const MultiExpenseChart = (expenseSummarySets: ExpenseSummary[][], collectBottomLevel: boolean) => {
+    const height = 300;
+    let series: any = [];
+    let options: any = {};
+    let categories: string[] = [];
+    let categoryForAccount: { [account: string]: string } = {};
+    for (const expenseSummaries of expenseSummarySets) {
+        for (const expense of expenseSummaries) {
+            let category = expense.account.split(':').slice(0, 2).join(':');
+            if (collectBottomLevel && category == expense.account) {
+                category = "Expenses:Other";
+            } else if (!collectBottomLevel) {
+                category = expense.account;
+            }
+            if (!categories.includes(category)) {
+                categories.push(category);
+            }
+            categoryForAccount[expense.account] = category;
+        }
+    }
+
+    let dateNames = Array.from({ length: expenseSummarySets.length }, (_, i) => `2024/${i+1}/01`);
+
+    while (expenseSummarySets.length > 0 && expenseSummarySets[0].length === 0) {
+        expenseSummarySets.shift();
+        dateNames.shift();
+    }
+
+    for (const category of categories) {
+        series.push({ name: category, data: Array.from({ length: expenseSummarySets.length }, () => 0) });
+    }
+
+
+    let i = 0;
+    for (const expenseSummaries of expenseSummarySets) {
+        for (const expense of expenseSummaries) {
+            const index = categories.indexOf(categoryForAccount[expense.account]);
+            series[index]["data"][i] += expense.amount;
+        }
+        i++;
+    }
+
+    options = {
+        chart: {
+            type: 'bar',
+            height: height,
+            stacked: true,
+            toolbar: {
+                show: true
+            },
+            zoom: {
+                enabled: false
+            }
+        },
+        dataLabels: {
+            formatter: expenseSummarySets.length === 0 ? (_1: any, _2: any) => "no data" : dollarFormatter,
+            style: {
+                fontSize: '13px',
+                fontWeight: 450
+            },
+        },
+        plotOptions: {
+            bar: {
+                horizontal: false,
+                borderRadius: 10,
+                borderRadiusApplication: 'end', // 'around', 'end'
+                borderRadiusWhenStacked: 'last', // 'all', 'last'
+                dataLabels: {
+                    position: 'center',
+                    total: {
+                        enabled: true,
+                        style: {
+                            fontSize: '13px',
+                            fontWeight: 450
+                        },
+                    },
+                }
+            },
+        },
+        xaxis: {
+            categories: dateNames,
+            // labels: { formatter: (s: string) => s.split(':').slice(-1)[0] }
+        },
+        yaxis: {
+            labels: { formatter: expenseSummarySets.length === 0 ? (_1: any, _2: any) => "no data" : dollarFormatter }, // two decimal points
+        },
+        legend: {
+            formatter: (s: string) => s.split(':').slice(-1)[0]
+        },
+        fill: {
+            opacity: 1
+        }
+    };
+
+    if (series.length === 0) {
+        series.push({ name: "", data: [] });
+    }
+
+    console.debug(options, series);
+
+    return (
+        <div>
+            <div id="chart">
+                <ReactApexChart options={options as ApexOptions} series={series} type={"bar"} height={height} />
+            </div>
+            <div id="html-dist"></div>
+        </div>
+    );
+}
+
 interface DashboardProps {
     exePath: string;
     filePath: string;
 }
 
-const Dashboard: React.FC<DashboardProps> = ({ exePath, filePath }) => {
+const SingleExpenseChart: React.FC<DashboardProps> = ({ exePath, filePath }) => {
     const [output, setOutput] = useState<ExpenseSummary[]>([]);
     const [period, setPeriod] = useState<PeriodType>(PeriodType.Month);
     const [startDate, setStartDate] = useState<MyDate>(fromMoment(moment().startOf(period.toLowerCase() as moment.unitOfTime.StartOf)));
@@ -182,18 +291,54 @@ const Dashboard: React.FC<DashboardProps> = ({ exePath, filePath }) => {
         getOutput();
     }, [startDate]);
 
+    return <div>
+        <div className="flex space-x-2 mb-4 justify-end align-center">
+            <MyDatePicker mode={period} date={toMoment(startDate).toDate()} setDate={(date: Date) => { setStartDate(fromMoment(moment(date))) }} />
+            <MultiButton
+                options={['Week', 'Month', 'Year']}
+                selectedOption={period}
+                onSelect={(newPeriod: string) => { setPeriod(newPeriod as PeriodType); setStartDate(fromMoment(toMoment(startDate).startOf(newPeriod.toLowerCase() as moment.unitOfTime.StartOf))) }}
+            />
+            <SettingsButton options={settingsButtonOptions} />
+        </div>
+        <div key={chartKey}>
+            {ExpenseChart(output, barChart, collectBottomLevel)}
+        </div>
+    </div>;
+};
+
+const MonthlyExpenseChart: React.FC<DashboardProps> = ({ exePath, filePath }) => {
+    const [output, setOutput] = useState<ExpenseSummary[][]>([]);
+
+    useEffect(() => {
+        async function getOutput() {
+            const promises = [];
+            let date: MyDate = { year: 2024, month: 1, day: 1 };
+            for (let i = 0; i < 12; i++) {
+                const next = spanNext(date, PeriodType.Month);
+                promises.push(query_balance_range(exePath, filePath, formatDate(date), formatDate(next)));
+                date = next;
+            }
+            const allOutputs = await Promise.all(promises);
+            setOutput(allOutputs.map((expenses: ExpenseSummary[]) => expenses.filter((expense: ExpenseSummary) => expense.account.startsWith("Expenses:"))));
+        }
+        getOutput();
+    }, []);
+
+    return <div>
+        {MultiExpenseChart(output, false)}
+    </div>;
+};
+
+const Dashboard: React.FC<DashboardProps> = ({ exePath, filePath }) => {
     // skeleton layout with flex grid, two rows, one with two div containers and one with three.
     return <div className="min-h-screen bg-gray-50 p-4">
         {/* First row - two cards */}
         <div className="flex gap-4 mb-4">
-            <div className="flex-1 bg-white rounded-lg shadow-md p-6">
-                {/* First card content */}
-                <div className="h-64">Card 1</div>
-            </div>
 
             <div className="flex-1 bg-white rounded-lg shadow-md p-6">
                 {/* Second card content */}
-                <div className="h-64">Card 2</div>
+                <MonthlyExpenseChart exePath={exePath} filePath={filePath} />
             </div>
         </div>
 
@@ -204,42 +349,11 @@ const Dashboard: React.FC<DashboardProps> = ({ exePath, filePath }) => {
                 <div className="h-48">Card 3</div>
             </div>
 
-            {/* <div className="flex-1 bg-white rounded-lg shadow-md p-6">
-                <div className="h-48">Card 4</div>
-            </div> */}
-
             <div className="basis-[70%] bg-white rounded-lg shadow-md p-6">
-                <div>
-                    <div className="flex space-x-2 mb-4 justify-end align-center">
-                        <MyDatePicker mode={period} date={toMoment(startDate).toDate()} setDate={(date: Date) => { setStartDate(fromMoment(moment(date))) }} />
-                        <MultiButton
-                            options={['Week', 'Month', 'Year']}
-                            selectedOption={period}
-                            onSelect={(newPeriod: string) => { setPeriod(newPeriod as PeriodType); setStartDate(fromMoment(toMoment(startDate).startOf(newPeriod.toLowerCase() as moment.unitOfTime.StartOf))) }}
-                        />
-                        <SettingsButton options={settingsButtonOptions} />
-                    </div>
-                    <div key={chartKey}>
-                        {ApexChart(output, barChart, collectBottomLevel)}
-                    </div>
-                </div>
+                <SingleExpenseChart exePath={exePath} filePath={filePath} />
             </div>
         </div>
     </div>
-    // return <div>
-    //     <div className="flex space-x-2 mb-4 justify-end align-center">
-    //         <MyDatePicker mode={period} date={toMoment(startDate).toDate()} setDate={(date: Date) => { setStartDate(fromMoment(moment(date))) }} />
-    //         <MultiButton
-    //             options={['Week', 'Month', 'Year']}
-    //             selectedOption={period}
-    //             onSelect={(newPeriod: string) => { setPeriod(newPeriod as PeriodType); setStartDate(fromMoment(toMoment(startDate).startOf(newPeriod.toLowerCase() as moment.unitOfTime.StartOf))) }}
-    //         />
-    //         <SettingsButton options={settingsButtonOptions} />
-    //     </div>
-    //     <div key={chartKey}>
-    //         {ApexChart(output, barChart, collectBottomLevel)}
-    //     </div>
-    // </div>
 };
 
 export { Dashboard };
